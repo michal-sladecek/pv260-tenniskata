46d2eb3c2b3d42736ace14838a897be11ab4b2d0
Michal Sladecek <469434@mail.muni.cz>
2021-04-15 19:44:54 +0200

    Each player starts with 0 points

---
+++ src/main/java/Tennis.java
@@ -0,0 +1,15 @@
+public class Tennis {
+    int scoreA=0;
+    int scoreB=0;
+
+    public Tennis() {
+    }
+
+    public int getScoreA() {
+        return scoreA;
+    }
+
+    public int getScoreB() {
+        return scoreB;
+    }
+}
