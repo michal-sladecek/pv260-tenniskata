8c1e566b6663aaa85b9736888d237c382bc4dc44
AdamRepasky <57711080+AdamRepasky@users.noreply.github.com>
2021-04-20 12:07:49 +0200

    implemented isWinner methods

--- src/main/java/Tennis.java
+++ src/main/java/Tennis.java
@@ -3,6 +3,7 @@ import java.util.HashMap;
 public class Tennis {
     int scoreA=0;
     int scoreB=0;
+    Winner winner = Winner.NONE;
     private HashMap<Integer, Integer> nextScore = new HashMap<>();
 
 
@@ -26,10 +27,26 @@ public class Tennis {
     public void scoredA(){
         if(nextScore.containsKey(scoreA))
             scoreA = nextScore.get(scoreA);
+        if(scoreA == 40 && scoreB != 40)
+            winner = Winner.A;
     }
 
     public void scoredB(){
         if(nextScore.containsKey(scoreB))
             scoreB = nextScore.get(scoreB);
+        if(scoreA != 40 && scoreB == 40)
+            winner = Winner.B;
+    }
+
+    public boolean isAWinner() {
+        return winner == Winner.A;
+    }
+    public boolean isBWinner() {
+        return winner == Winner.B;
+    }
+    enum Winner {
+        NONE,
+        A,
+        B
     }
 }
